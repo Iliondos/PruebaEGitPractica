51d8cdd535246d08730117d00365a39367e5be25
Iliondos <iliondos@hotmail.com>
2013-03-19 20:50:01 +0100

    Primer commit de prueba

---
+++ src/miPruebaGit/ProbandoGit.java
@@ -0,0 +1,15 @@
+package miPruebaGit;
+
+public class ProbandoGit {
+
+	/**
+	 * Clase para probar EGit
+	 * @author Olga
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Probando Git.");
+
+	}
+
+}
